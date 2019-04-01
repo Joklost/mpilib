a69a18539de823ca6947535ed031fd1c84412799
Jonas Kloster Jacobsen <joklost@gmail.com>
2019-04-01 11:11:04 +0200

    Changed spdlog header include
    Signed-off-by: Jonas Kloster Jacobsen <joklost@gmail.com>

--- include/mpilib/mpi.h
+++ include/mpilib/mpi.h
@@ -4,8 +4,7 @@
 #include <mpich/mpi.h>
 
 #include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/fmt/ostr.h>
 
 #include <mpilib/objectifier.h>
